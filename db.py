3d28c1f374d9a2c5789ecc845ae055fcb3594f64
ddujeong <qoddl159@nate.com>
2026-03-25 15:12:08 +0900

    수정

--- db.py
+++ db.py
@@ -9,7 +9,7 @@ class Database:
                 # host='localhost',
                 host='mariadb',  # cloudtype 사용 시
                 port=3306,   # cloudtype 사용 시
-                database='test1',  # test 데이터베이스 사용
+                database='test',  # test 데이터베이스 사용
                 user='root',
                 password='DB_PASSWORD',  # mariadb 설치 당시의 패스워드, 실제 환경에서는 보안을 위해 환경변수 등을 사용
                 charset='utf8mb4',
